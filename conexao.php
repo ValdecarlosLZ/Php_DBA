b70cb65c331f942fdd272469075e83ecc0a98c8c
ValdecarlosLZ <101909600+ValdecarlosLZ@users.noreply.github.com>
2023-04-25 21:34:32 -0400

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -3,6 +3,10 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $celular = $_POST['celular'];
 
+// $host = "localhost";
+// $db = "aulaphp";
+// $user = "root";
+
 $conexao = new PDO("mysql:host=localhost:3307;dbname=aulaphp", "root", "");
 
 $sql = "INSERT INTO contatos (nome,email,celular) VALUES (:nome,:email,:celular)" ;
